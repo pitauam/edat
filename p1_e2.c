10e8c7485be26dcbf325b42218e33f6781bc5f3f
Tibu <mariorodrima@gmail.com>
2026-02-16 17:02:36 +0000

    cambios a p1_e2

--- p1_e2.c
+++ p1_e2.c
@@ -6,6 +6,8 @@
 int main(){
 
     Radio *radio = NULL;
+    Music *cancion1 = NULL;
+    Music *cancion2 = NULL;
     int n_relation = 0;
     char descr1 [300] = "id:111 title:Paint It, Black artist:The Rolling Stones duration:202";
     char descr2 [300] = "id:222 title:Every Breath You Take artist:The Police duration:253";
@@ -13,6 +15,8 @@ int main(){
     radio = radio_init();
 
     if(radio == NULL){return -1;}
+    cancion1 = music_initFromString(descr1);
+    cancion2 = music_initFromString(descr2);
 
     if(!(radio_newMusic(radio, descr1))){
         radio_free(radio);    
@@ -24,30 +28,38 @@ int main(){
         return -1;
     }
     
-    if(!(radio_newRelation(radio, 222 , 111))){
+    if(!(radio_newRelation(radio, music_getId(cancion2) , music_getId(cancion1)))){
         radio_free(radio);
         return -1;
     }
     printf("Inserting radio recomendation 222 --> 111\n");
     printf("\n111-->222?");
 
-    if(radio_relationExists(radio, 111, 222)){
+    if(radio_relationExists(radio, music_getId(cancion1) , music_getId(cancion2))){
         printf("Yes\n");
     }else{
         printf("No\n");
     }
     printf("222-->111?");
-    if(radio_relationExists(radio, 222,111)){
+    if(radio_relationExists(radio, music_getId(cancion2) , music_getId(cancion1))){
         printf("Yes\n");
     }else{
         printf("No\n");
     }
     
-    if(n_relation = radio_getNumberOfRelationsFromId(radio, 111) == -1){
+    if( (n_relation = radio_getNumberOfRelationsFromId(radio, music_getId(cancion1))) == -1){
         return -1;
     }
-    printf("Number of connections from 111: %d\n", n_relation);
 
+    printf("Number of connections from %ld: %d\n",music_getId(cancion1), n_relation);
 
+    if( (n_relation = radio_getNumberOfRelationsFromId(radio, music_getId(cancion2))) == -1){
+        return -1;
+    }
+    printf("Number of connections from %ld: %d\n",music_getId(cancion2), n_relation);
+
+    radio_print(stdout, radio);
+
+    radio_free(radio);
 
 }
